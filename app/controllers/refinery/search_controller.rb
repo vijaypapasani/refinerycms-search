9d717703a5c8897a169389fa40cefea6a4a4aa69
Vijay Papasani <user@ubuntu.(none)>
2013-01-28 15:54:56 +0530

    search test

--- app/controllers/refinery/search_controller.rb
+++ app/controllers/refinery/search_controller.rb
@@ -3,9 +3,9 @@ module Refinery
 
   	# Display search results given the query supplied
     def show
-      @results = Refinery::SearchEngine.search(params[:query], params[:page])
+      @results = Refinery::SearchEngine.search(params[:query1], params[:page1])
 
-      present(@page = Refinery::Page.find_by_link_url("/search111"))
+      present(@page = Refinery::Page.find_by_link_url("/search"))
     end
 
   end
